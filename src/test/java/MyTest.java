c9bc0d4e38a86f9705f3e55ff93c126f9f98d12e
MaevaPereira <maeva.pereira@le-campus-numerique.fr>
2026-03-26 21:10:30 +0100

    Classe Test (fail)

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,6 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MyTest {
 
-
+    @Test
+    void testBonjour() {
+        Welcome welcome = new Welcome();
+        assertEquals("Bonjour!", welcome.getMessage());
+    } //test fail car il s'attend à "Bonjour!" mais en réalité c'est "Bonsoir" car il lit l'heure réelle du pc (20h)
 
 
 
